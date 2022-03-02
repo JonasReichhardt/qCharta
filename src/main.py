d9e7bbe823102cfbaa70631fccc9d66d320b91b9
Jonas Reichhardt <j.reichhardt@tig.at>
2022-03-02 11:17:01 +0100

    switch to python

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit.test.mock.backends import FakeBrooklyn
+
+path = '..\\benchmarks\\adder-3.qasm'
+qc = QuantumCircuit.from_qasm_file(path=path)
+qc_transpiled = transpile(qc, backend=FakeBrooklyn())
+filename = '..\\mapped\\adder-3.qasm'
+qc_transpiled.qasm(filename=filename)

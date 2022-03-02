1fed2afe1ac664cf40892e1bb6b9e88393c6583d
Jonas Reichhardt <j.reichhardt@tig.at>
2022-03-02 17:02:31 +0100

    add reference implementation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,100 @@
+from weakref import ref
 from qiskit import QuantumCircuit, transpile
 from qiskit.test.mock.backends import FakeBrooklyn
+from qiskit.converters import circuit_to_dag, dag_to_circuit
+from qiskit.transpiler import CouplingMap
+from qiskit.dagcircuit import DAGCircuit
+from qiskit.transpiler import Layout
+from qCharta import qCharta
+import os
+from coupling import couplingmap_brooklyn
 
-path = '..\\benchmarks\\adder-3.qasm'
-qc = QuantumCircuit.from_qasm_file(path=path)
-qc_transpiled = transpile(qc, backend=FakeBrooklyn())
-filename = '..\\mapped\\adder-3.qasm'
-qc_transpiled.qasm(filename=filename)
+inputdir = "..\\benchmarks\\"
+outputdir = "..\\mapped\\"
+
+def main():
+    # get all qasm files of directory
+    files = os.listdir(inputdir)
+    files = list(filter(lambda path: path.endswith(".qasm"),files))
+
+    result = do_benchmark(files)
+
+    print("Cost of reference implementation: "+str(result[0]))
+
+def do_benchmark(files, ref_benchmark = False):
+    reference_results = []
+    own_results = []
+    reference_cost = 0
+    own_cost = 0
+    quantum_circuits = {}
+
+    print("[Step 1: parse qasm files]")
+    for filename in files:
+        quantum_circuits["filename"]=QuantumCircuit.from_qasm_file(path=inputdir+filename)
+    
+    if ref_benchmark:
+        print("[Step 2: transpile trivial mapping with basic swaps]")
+        for name, circuit in quantum_circuits.items():
+            transpiled = transpile(circuit, backend=FakeBrooklyn(),routing_method="basic")
+
+            cost = get_circuit_cost(transpiled)
+            reference_results.append(cost)
+            reference_cost = reference_cost+cost
+            
+            transpiled.qasm(filename=outputdir+"reference\\"+name)
+    else:
+        print("[skip Step2]")
+    
+    print("[Step 3: transpile with qCharta]")
+    for name, circuit in quantum_circuits.items():
+        # create coupling map
+        coupling_map = CouplingMap(couplingmap_brooklyn)
+
+        # create transpiler with coupling map
+        transpiler = qCharta(coupling_map,100)
+        
+        # convert circuit to dag, transpile and convert back
+        dag = circuit_to_dag(circuit)
+        mapped_dag = transpiler.run(dag)
+        mapped_qc = dag_to_circuit(mapped_dag)
+
+        filecontent = mapped_qc.qasm()
+        filecontent = filecontent.replace('\n', '\n' + get_layout_description_comment(transpiler.initial_layout, dag) + '\n', 1)
+
+        with open(outputdir+"qCharta\\"+name, "w+") as file:
+            file.write(filecontent)
+        file.close()
+
+        
+
+    return [reference_cost,reference_results,own_cost,own_results]
+
+# author: Elias Foramitti
+def get_circuit_cost(qc: QuantumCircuit) -> int:
+    instructions = [i[0] for i in qc]
+    cost = 0
+    for i, inst in enumerate(instructions):
+        if inst.name == 'sx' or inst.name == 'x':
+            cost += 1
+        elif inst.name == 'cx':
+            cost += 10
+        elif inst.name == 'swap':
+            cost += 30
+        elif (inst.name != 'rz' and inst.name != 'measure' and inst.name != 'barrier'):
+            print(f"{i}th instruction '{inst.name}' not allowed")
+    return cost
+
+# author: Elias Foramitti
+def get_layout_description_comment(layout: Layout, dag: DAGCircuit):
+    physical_qbits = []
+    virtual_bit_mapping = layout.get_virtual_bits()
+    # one could directly take layout.get_virtual_bits().values(), 
+    # but that would not necessarily preserve the original ordering 
+    # of virtual qubits resulting in a wrong layout description
+    for qreg in dag.qregs.values():
+        for qbit in qreg:
+            physical_qbits.append(virtual_bit_mapping[qbit])
+    return '// i ' + ' '.join(str(i) for i in physical_qbits)
+
+if __name__=="__main__":
+    main()
---
+++ src/qCharta.py
@@ -0,0 +1,88 @@
+from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.transpiler import Layout, TranspilerError
+from qiskit.circuit.library import SwapGate
+from qiskit.dagcircuit import DAGCircuit
+from qiskit import QuantumRegister
+import random
+
+
+class qCharta(TransformationPass):
+
+    def __init__(self,
+                 coupling_map, seed):
+        super().__init__()
+        self.coupling_map = coupling_map
+        self.seed = seed
+        random.seed(seed)
+
+    def create_random_layout(self,dag):
+        reg = QuantumRegister(len(self.coupling_map.physical_qubits) - len(dag.qubits), 'r')
+        dag.add_qreg(reg)
+
+        # generate random map with register size
+
+        return 1
+
+    def run(self, dag):
+        self.initial_layout = self.create_random_layout(dag)
+
+        new_dag = DAGCircuit()
+        for qreg in dag.qregs.values():
+            new_dag.add_qreg(qreg)
+        for creg in dag.cregs.values():
+            new_dag.add_creg(creg)
+
+
+        if self.initial_layout is None:
+            if self.property_set["layout"]:
+                self.initial_layout = self.property_set["layout"]
+            else:
+                self.initial_layout = Layout.generate_trivial_layout(*dag.qregs.values())
+
+        if len(dag.qubits) != len(self.initial_layout):
+            raise TranspilerError('The layout does not match the amount of qubits in the DAG')
+
+        if len(self.coupling_map.physical_qubits) != len(self.initial_layout):
+            raise TranspilerError(
+                "Mappers require to have the layout to be the same size as the coupling map")
+
+        canonical_register = dag.qregs['q']
+        trivial_layout = Layout.generate_trivial_layout(canonical_register)
+        current_layout = trivial_layout.copy()
+
+        for layer in dag.serial_layers():
+            subdag = layer['graph']
+
+            for gate in subdag.two_qubit_ops():
+                physical_q0 = current_layout[gate.qargs[0]]
+                physical_q1 = current_layout[gate.qargs[1]]
+                if self.coupling_map.distance(physical_q0, physical_q1) != 1:
+                    # Insert a new layer with the SWAP(s).
+                    swap_layer = DAGCircuit()
+                    swap_layer.add_qreg(canonical_register)
+
+                    path = self.coupling_map.shortest_undirected_path(physical_q0, physical_q1)
+                    for swap in range(len(path) - 2):
+                        connected_wire_1 = path[swap]
+                        connected_wire_2 = path[swap + 1]
+
+                        qubit_1 = current_layout[connected_wire_1]
+                        qubit_2 = current_layout[connected_wire_2]
+
+                        # create the swap operation
+                        swap_layer.apply_operation_back(SwapGate(),
+                                                        qargs=[qubit_1, qubit_2],
+                                                        cargs=[])
+
+                    # layer insertion
+                    order = current_layout.reorder_bits(new_dag.qubits)
+                    new_dag.compose(swap_layer, qubits=order)
+
+                    # update current_layout
+                    for swap in range(len(path) - 2):
+                        current_layout.swap(path[swap], path[swap + 1])
+
+            order = current_layout.reorder_bits(new_dag.qubits)
+            new_dag.compose(subdag, qubits=order)
+
+        return new_dag
